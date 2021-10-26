db8714cda2aef94d9586ecfc5d049d28e85753e1
AlexandrKozlov1980 <A362132@yandex.ru>
2021-10-26 20:19:11 +0300

    TEAMUP-8 исправил репозиторий

--- teamup-core/src/main/java/ru/team/up/core/repositories/InterestsRepository.java
+++ teamup-core/src/main/java/ru/team/up/core/repositories/InterestsRepository.java
@@ -3,5 +3,5 @@ package ru.team.up.core.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.team.up.core.entity.Interests;
 
-public interface IntrestsRepository extends JpaRepository<Interests, Long> {
+public interface InterestsRepository extends JpaRepository<Interests, Long> {
 }
