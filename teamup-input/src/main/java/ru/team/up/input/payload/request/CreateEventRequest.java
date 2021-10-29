2935a8fbaf22f6989a1e58f58fff02e02069535c
Pavel Kondrashov <theicy201@gmail.com>
2021-10-30 02:19:14 +0700

    TEAMUP-10: Изменил аннотации в request и response классах

--- teamup-input/src/main/java/ru/team/up/input/payload/request/CreateEventRequest.java
+++ teamup-input/src/main/java/ru/team/up/input/payload/request/CreateEventRequest.java
@@ -1,19 +1,15 @@
 package ru.team.up.input.payload.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Builder;
+import lombok.Data;
 import ru.team.up.input.entity.Event;
 
 /**
  * @author Pavel Kondrashov on 27.10.2021
  */
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
+@Data
+@Builder
 public class CreateEventRequest {
     private Event event;
 }
--- teamup-input/src/main/java/ru/team/up/input/payload/response/CreateEventResponse.java
+++ teamup-input/src/main/java/ru/team/up/input/payload/response/CreateEventResponse.java
@@ -1,19 +1,15 @@
 package ru.team.up.input.payload.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Builder;
+import lombok.Value;
 
 /**
  * @author Pavel Kondrashov on 27.10.2021
  */
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
+@Value
+@Builder
 public class CreateEventResponse {
-    private String message;
-    private String status;
+    String message;
+    String status;
 }
