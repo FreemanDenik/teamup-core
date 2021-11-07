ac0ac3ce893ba225b5affa0e8c5a5fd2fd70a433
tihonov10101 <tihonov10101@gmail.com>
2021-11-07 16:47:48 +0500

    Добавил новый модуль, настроил многомодульность,контекст, некоторый код заккоменчен т.к. есть конфликты

--- teamup-app/src/main/java/ru/team/up/app/TeamupAppApplication.java
+++ teamup-app/src/main/java/ru/team/up/app/TeamupAppApplication.java
@@ -1,17 +1,20 @@
-package ru.team.up.core;
+package ru.team.up.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@PropertySource("classpath:db.properties")
-@EnableJpaRepositories
-public class TeamupCoreApplication {
+@SpringBootApplication(scanBasePackages = "ru.team.up*")
+@PropertySource({
+        "classpath:db.properties",
+        "classpath:auth.properties",
+        "classpath:external.properties"
+})
+public class TeamupAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TeamupCoreApplication.class, args);
+        SpringApplication.run(TeamupAppApplication.class, args);
     }
 
 }
