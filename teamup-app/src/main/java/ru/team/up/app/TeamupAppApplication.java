288d9f9e86b8418f0e64d524c149c7655cbe5323
tihonov10101 <tihonov10101@gmail.com>
2021-11-08 00:32:06 +0500

    не отображается view если сканировать все пакеты

--- teamup-app/src/main/java/ru/team/up/app/TeamupAppApplication.java
+++ teamup-app/src/main/java/ru/team/up/app/TeamupAppApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication(scanBasePackages = "ru.team.up")
+@SpringBootApplication(scanBasePackages = "ru.team.up.auth")
 @EnableJpaRepositories("ru.team.up*")
 @EntityScan("ru.team.up*")
 @PropertySource({
@@ -17,7 +17,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
         "classpath:auth.properties",
         "classpath:external.properties"
 })
-@ComponentScan("ru.team.up.auth")
 public class TeamupAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(TeamupAppApplication.class, args);
