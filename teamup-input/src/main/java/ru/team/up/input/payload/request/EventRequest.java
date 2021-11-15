d135c9edc2bbccdb7f1b920eb4082deddca8b378
Pavel Kondrashov <theicy201@gmail.com>
2021-11-15 18:37:06 +0700

    TEAMUP-31: Удалена сущность Event. JavaDoc для request и response

--- teamup-input/src/main/java/ru/team/up/input/payload/request/EventRequest.java
+++ teamup-input/src/main/java/ru/team/up/input/payload/request/EventRequest.java
@@ -5,11 +5,17 @@ import lombok.Data;
 import ru.team.up.core.entity.Event;
 
 /**
+ * Класс для запроса сущности мероприятия
+ *
  * @author Pavel Kondrashov
  */
 
 @Data
 @Builder
 public class EventRequest {
+
+    /**
+     * Сущность мероприятия
+     */
     private Event event;
 }
