842d3954800a8925c30b8d8d89e9ad3f591dd82f
Alexander Popov <alexandpopov@yandex.ru>
2021-12-19 14:05:29 +0300

    Тестирование EventController + Проверены и добавлены тесты для EventController.java (package ru.team.up.input.controller.privateController). В контроллере добавлена проверка обязательных значений. Дописаны тесты для неверных значений в   ru.team.up.input.controllerPrivateTest.TeamupInputEventPrivateControllerTests.

--- teamup-input/src/main/java/ru/team/up/input/controller/privateController/EventController.java
+++ teamup-input/src/main/java/ru/team/up/input/controller/privateController/EventController.java
@@ -1,17 +1,15 @@
 package ru.team.up.input.controller.privateController;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.team.up.core.entity.Event;
 import ru.team.up.core.service.EventService;
 
+import javax.persistence.PersistenceException;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
@@ -24,8 +22,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping(value = "private/event", produces = MediaType.APPLICATION_JSON_VALUE)
-@Tag(name = "Private Event RestController", description = "Private Event API")
+@RequestMapping("private/event")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class EventController {
     private EventService eventService;
@@ -35,7 +32,6 @@ public class EventController {
      * в теле ResponseEntity
      */
     @GetMapping
-    @Operation(summary = "Получить все мероприятия")
     public ResponseEntity<List<Event>> getAllEvents() {
         log.debug("Старт метода ResponseEntity<List<Event>> getAllEvents()");
 
@@ -51,7 +47,6 @@ public class EventController {
      * в теле ResponseEntity
      */
     @GetMapping("/{id}")
-    @Operation(summary = "Получить одно мероприятие")
     public ResponseEntity<Event> getOneEvent(@PathVariable Long id) {
         log.debug("Старт метода ResponseEntity<Event> getOneEvent(@PathVariable Long id) с параметром {}", id);
 
@@ -67,14 +62,18 @@ public class EventController {
      * в теле ResponseEntity
      */
     @PostMapping
-    @Operation(summary = "Создать мероприятие")
-    public ResponseEntity<Event> createEvent(@RequestParam String event, @RequestBody @NotNull Event eventCreate) {
+    public ResponseEntity<Event> createEvent(@RequestBody @NotNull Event eventCreate) {
         log.debug("Старт метода ResponseEntity<Event> createEvent(@RequestBody @NotNull Event event) с параметром {}", eventCreate);
 
-        ResponseEntity<Event> responseEntity = new ResponseEntity<>(eventService.saveEvent(eventCreate), HttpStatus.CREATED);
-        log.debug("Получили ответ {}", responseEntity);
-
-        return responseEntity;
+        try {
+            ResponseEntity<Event> responseEntity = new ResponseEntity<>(eventService.saveEvent(eventCreate), HttpStatus.CREATED);
+            log.debug("Получили ответ {}", responseEntity);
+            return responseEntity;
+        } catch (PersistenceException e) {
+            ResponseEntity<Event> responseEntity = new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+            log.debug("Получили ответ {}", responseEntity);
+            return responseEntity;
+        }
     }
 
     /**
@@ -83,14 +82,18 @@ public class EventController {
      * в теле ResponseEntity
      */
     @PatchMapping
-    @Operation(summary = "Изменить мероприятие")
     public ResponseEntity<Event> updateEvent(@RequestBody @NotNull Event event) {
         log.debug("Старт метода ResponseEntity<Event> updateEvent(@RequestBody @NotNull Event event) с параметром {}", event);
+        try {
+            ResponseEntity<Event> responseEntity = ResponseEntity.ok(eventService.saveEvent(event));
+            log.debug("Получили ответ {}", responseEntity);
+            return responseEntity;
 
-        ResponseEntity<Event> responseEntity = ResponseEntity.ok(eventService.saveEvent(event));
-        log.debug("Получили ответ {}", responseEntity);
-
-        return responseEntity;
+        } catch (PersistenceException e) {
+            ResponseEntity<Event> responseEntity = new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+            log.debug("Получили ответ {}", responseEntity);
+            return responseEntity;
+        }
     }
 
     /**
@@ -98,7 +101,6 @@ public class EventController {
      * @return Объект ResponseEntity со статусом OK
      */
     @DeleteMapping("/{id}")
-    @Operation(summary = "Удалить мероприятие")
     public ResponseEntity<Event> deleteAdmin(@PathVariable Long id) {
         log.debug("Старт метода ResponseEntity<Event> deleteAdmin(@PathVariable Long id) с параметром {}", id);
 
