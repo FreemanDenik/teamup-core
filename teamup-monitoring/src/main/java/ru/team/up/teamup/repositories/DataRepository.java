380a648f23f02f2dd8696d29d76089db4a0fedf5
Nikita Rocjev <norcen@yandex.ru>
2022-02-01 23:13:31 +0300

    Правки для запуска со всеми модулями

--- teamup-monitoring/src/main/java/ru/team/up/teamup/repositories/DataRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.team.up.teamup.repositories;
-
-import lombok.Data;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DataRepository extends CrudRepository<Data, String> {
-}
--- teamup-monitoring/src/main/java/ru/team/up/teamup/service/DataService.java
+++
@@ -1,7 +0,0 @@
-package ru.team.up.teamup.service;
-
-import lombok.Data;
-
-public interface DataService {
-    Data saveMessage(Data data);
-}
