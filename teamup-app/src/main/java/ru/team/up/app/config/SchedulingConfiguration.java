f96ed590d28a07984da91a86ae33eea22f6f9a1a
mmr3501 <mmr3501@yandex.ru>
2022-02-19 02:50:35 +0300

    Изменил способ обход листа с городами, навесил аннотации @EnableAsync и @Async в классе-конфигурации SchedulingConfiguration и cityDefaultCreator бине соответственно. Не уверен, действительно ли аннотации выполняют свой функционал асинхрона, но они заставили меня думать, что приложение стало запускаться быстрее))

--- teamup-app/src/main/java/ru/team/up/app/config/SchedulingConfiguration.java
+++ teamup-app/src/main/java/ru/team/up/app/config/SchedulingConfiguration.java
@@ -2,11 +2,13 @@ package ru.team.up.app.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
 @ConditionalOnProperty(name = "scheduling.enable", matchIfMissing = true)
+@EnableAsync
 class SchedulingConfiguration {
 
 }
