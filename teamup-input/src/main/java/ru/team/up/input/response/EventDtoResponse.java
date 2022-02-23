ecc28e3056bbdba635f2c4b49b0a709ab909122f
ASUS <kdv1990kdv@bk.ru>
2022-02-23 15:08:11 +0300

    Проработать endpoint /public/event/id/{id} GET

---
+++ teamup-input/src/main/java/ru/team/up/input/response/EventDtoResponse.java
@@ -0,0 +1,16 @@
+package ru.team.up.input.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.team.up.dto.EventDto;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class EventDtoResponse {
+
+    private EventDto eventDto;
+}
