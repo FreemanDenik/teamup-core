2654120b157ba4e1a07c282b0aaf2f9bbdf9610f
Irik Kashapov <Irikan88@gmail.com>
2022-03-07 13:41:45 +0500

    bugfix/TEAMUP-21 поправил bug с политикой CORS

--- teamup-auth/src/main/java/ru/team/up/auth/config/SecurityConfigToken.java
+++ teamup-auth/src/main/java/ru/team/up/auth/config/SecurityConfigToken.java
@@ -16,7 +16,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import ru.team.up.auth.config.jwt.JwtFilter;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -38,6 +43,8 @@ public class SecurityConfigToken extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.
+                cors().
+                and().
                 httpBasic().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
@@ -65,6 +72,17 @@ public class SecurityConfigToken extends WebSecurityConfigurerAdapter {
     @Bean
     public AuthenticationManager customAuthenticationManager() throws Exception {
         return authenticationManager();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins
+                (Arrays.asList("http://localhost:8080/swagger-ui/index.html?configUrl=/v3/api-docs/swagger-config#/"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
 
