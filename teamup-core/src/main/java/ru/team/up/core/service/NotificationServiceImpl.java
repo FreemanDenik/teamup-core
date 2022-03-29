dd18431e3d57c893a4e731898887dca884e46c9e
dmitrykoryanov <dmitry.koryanov@yandex.ru>
2022-03-29 11:46:17 +0300

    TEAMUP-27 Проработать до конца систему уведомлений * Добавлен зависиость Spring WebFlux для вызова сервиса отправки уведомлений * Добавлен конфигурационный класс для создания WebClient-а * Добавлена реализация вызова сервиса отправки уведомлений * Добавлен конфигурациооный файл notification.properties для указания хоста и порта сервиса по отправке уведомлений

--- teamup-core/src/main/java/ru/team/up/core/service/NotificationServiceImpl.java
+++ teamup-core/src/main/java/ru/team/up/core/service/NotificationServiceImpl.java
@@ -1,11 +1,48 @@
 package ru.team.up.core.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import ru.team.up.dto.NotificationDto;
 
+/**
+ * Author Nail Faizullin, Dmitry Koryanov
+ * сервис-класс для отправки уведомлений
+ */
+
+@Slf4j
+@Service
 public class NotificationServiceImpl implements NotificationService{
+
+    @Autowired
+    WebClient webClient;
 
     @Override
     public void sendNotification(NotificationDto notificationDto) {
+        Mono<NotificationDto> notificationDtoMono = Mono.just(notificationDto);
+
+        log.debug("Отправляем POST запрос на помещение уведомления {} в очередь на отправку", notificationDto);
+
+        webClient
+                .post()
+                .uri("/notification")
+                .body(notificationDtoMono, NotificationDto.class)
+                .exchangeToMono(response -> {
+                    if (response.statusCode().equals(HttpStatus.CREATED)) {
+                        return response.bodyToMono(NotificationDto.class);
+                    }
+                    else {
+                        log.debug("При отправке POST запроса на помещение уведомления {} в очередь на отправку " +
+                                        "возникла ошибка. Код ответа {}",
+                                notificationDto, response.statusCode());
+                        return Mono.empty();
+                    }
+                })
+            .subscribe(resultNotificationDto -> {log.debug("Уведомление {} успешно помещено в очередь на отправку",
+                    resultNotificationDto);});
 
     }
 }
