770b93f73078674bf3c468fe754a767cce8c9e30
Yura Katkov <94790420+JZawR@users.noreply.github.com>
2022-05-05 12:56:26 +0300

    feature/TEAMUP-31 добавил/исправил тесты в teamup-input

--- teamup-input/src/test/java/ru/team/up/input/controller/publicController/EventRestControllerPublicTest.java
+++ teamup-input/src/test/java/ru/team/up/input/controller/publicController/EventRestControllerPublicTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
@@ -15,6 +16,8 @@ import ru.team.up.core.entity.*;
 import ru.team.up.core.monitoring.service.MonitorProducerService;
 import ru.team.up.dto.ControlDto;
 import ru.team.up.dto.ReportDto;
+import ru.team.up.input.exception.EventCheckException;
+import ru.team.up.input.exception.EventCreateRequestException;
 import ru.team.up.input.payload.request.EventRequest;
 import ru.team.up.input.payload.request.JoinRequest;
 import ru.team.up.input.payload.request.UserRequest;
@@ -26,8 +29,8 @@ import java.time.LocalDateTime;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @Slf4j
 @RunWith(MockitoJUnitRunner.class)
@@ -42,47 +45,46 @@ class EventRestControllerPublicTest {
     @Mock
     private MonitorProducerService monitoringProducerService;
 
+    @Spy
     @InjectMocks
     private EventRestControllerPublic eventRestControllerPublic =
             new EventRestControllerPublic(eventServiceRest, wordMatcher, monitoringProducerService);
 
-    List<Event> events;
-    JoinRequest joinRequest;
-    UserRequest userRequest;
-    EventRequest eventRequest2;
-    Event event2;
-    Event event;
-    Status status;
-    User testUser2;
-    User testUser;
-    Interests programming;
-    EventType eventType;
-    ReportDto reportDto;
-
-    User securityUser;
-    Authentication authentication;
-    SecurityContext securityContext;
+    private List<Event> events;
+    private JoinRequest joinRequest;
+    private JoinRequest emptyJoinRequest;
+    private UserRequest userRequest;
+    private EventRequest eventRequest2;
+    private Event event2;
+    private Event event;
+    private User testUser2;
+    private EventType eventType;
 
     @BeforeEach
     private void setUpEntity() {
         MockitoAnnotations.openMocks(this);
 
-        securityUser = mock(User.class);
-        authentication = mock(Authentication.class);
-        securityContext = mock(SecurityContext.class);
-
+        emptyJoinRequest = null;
         events = new LinkedList<>();
+        User securityUser = mock(User.class);
+        Authentication authentication = mock(Authentication.class);
+        SecurityContext securityContext = mock(SecurityContext.class);
+
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+        when(SecurityContextHolder.getContext().getAuthentication().getPrincipal()).thenReturn(securityUser);
 
         eventType = EventType.builder()
+                .id(1L)
                 .type("type")
                 .build();
 
-        programming = Interests.builder()
+        Interests programming = Interests.builder()
                 .title("Programming")
                 .shortDescription("Like to write programs in java")
                 .build();
 
-        testUser = User.builder()
+        User testUser = User.builder()
                 .id(1L)
                 .firstName("Aleksey")
                 .lastName("Tkachenko")
@@ -93,7 +95,7 @@ class EventRestControllerPublicTest {
                 .accountCreatedTime(LocalDate.now())
                 .lastAccountActivity(LocalDateTime.now())
                 .city("Moscow")
-                .birthday(LocalDate.of (1979, 1, 20))
+                .birthday(LocalDate.of(1979, 1, 20))
                 .aboutUser("Studying to be a programmer.")
                 .userInterests(Collections.singleton(programming))
                 .build();
@@ -114,11 +116,12 @@ class EventRestControllerPublicTest {
                 .userInterests(Collections.singleton(programming))
                 .build();
 
-        status = Status.builder()
+        Status status = Status.builder()
                 .status("status")
                 .build();
 
         event = Event.builder()
+                .id(1L)
                 .eventName("eventName")
                 .descriptionEvent("descriptionEvent")
                 .placeEvent("placeEvent")
@@ -132,6 +135,7 @@ class EventRestControllerPublicTest {
                 .build();
 
         event2 = Event.builder()
+                .id(2L)
                 .eventName("eventName2")
                 .descriptionEvent("descriptionEvent2")
                 .placeEvent("placeEvent2")
@@ -157,18 +161,13 @@ class EventRestControllerPublicTest {
                 .user(testUser)
                 .build();
 
-
         events.add(event);
         events.add(event2);
-
     }
 
     @Test
     void getAllEvents() {
         when(eventServiceRest.getAllEvents()).thenReturn(events);
-        when(securityContext.getAuthentication()).thenReturn(authentication);
-        SecurityContextHolder.setContext(securityContext);
-        when(SecurityContextHolder.getContext().getAuthentication().getPrincipal()).thenReturn(securityUser);
         // Все ок
         assertEquals(2, eventRestControllerPublic.getAllEvents().getEventDtoList().size());
         // Не ок
@@ -177,41 +176,135 @@ class EventRestControllerPublicTest {
 
     @Test
     void findEventById() {
+        when(eventServiceRest.getEventById(1L)).thenReturn(event);
+        // OK
+        assertEquals(1L, eventRestControllerPublic.findEventById(1L).getEventDto().getId());
+        // Not OK
+        assertNotEquals(2L, eventRestControllerPublic.findEventById(1L).getEventDto().getId());
     }
 
     @Test
     void getAllEventByCity() {
+        when(eventServiceRest.getAllEventsByCity("Краснодар")).thenReturn(events);
+        // OK
+        assertEquals(2, eventRestControllerPublic.getAllEventByCity("Краснодар").getEventDtoList().size());
+        // Not OK
+        assertNotEquals(5, eventRestControllerPublic.getAllEventByCity("Краснодар").getEventDtoList().size());
+        // Неверный запрос
+        assertNotEquals(2, eventRestControllerPublic.getAllEventByCity("Футбол").getEventDtoList().size());
     }
 
     @Test
     void findEventsByName() {
+        when(eventServiceRest.getEventByName("Event")).thenReturn(events);
+        // OK
+        assertEquals(2, eventRestControllerPublic.findEventsByName("Event").getEventDtoList().size());
+        // Not OK
+        assertNotEquals(4,
+                eventRestControllerPublic.findEventsByName("Event").getEventDtoList().size());
+        // Неверный запрос
+        assertNotEquals(2,
+                eventRestControllerPublic.findEventsByName("WrongEventName").getEventDtoList().size());
     }
 
     @Test
     void findEventsByAuthor() {
+        when(eventServiceRest.getAllEventsByAuthor(1L)).thenReturn(events);
+        // OK
+        assertEquals(200, eventRestControllerPublic.findEventsByAuthor(userRequest).getStatusCodeValue());
+        // Not OK
+        assertEquals(204,
+                eventRestControllerPublic.findEventsByAuthor(new UserRequest(testUser2)).getStatusCodeValue());
+        // Неверный запрос
+        assertThrows(NullPointerException.class,
+                ()-> eventRestControllerPublic.findEventsByAuthor(new UserRequest()).getStatusCodeValue());
     }
 
     @Test
     void findEventsByType() {
+        when(eventServiceRest.getAllEventsByEventType(eventType)).thenReturn(events);
+        // OK
+        assertEquals(200, eventRestControllerPublic.findEventsByType(eventType).getStatusCodeValue());
+        // Неверный запрос
+        assertEquals(204, eventRestControllerPublic.findEventsByType(new EventType()).getStatusCodeValue());
     }
 
     @Test
     void createEvent() {
+        when(eventServiceRest.saveEvent(event2)).thenReturn(event2);
+        // OK
+        assertEquals(201, eventRestControllerPublic.createEvent(eventRequest2).getStatusCodeValue());
+        // Неверный запрос
+        assertThrows(NullPointerException.class, ()-> eventRestControllerPublic.createEvent(new EventRequest()));
+        // Нет плохих слов
+        when(wordMatcher.detectBadWords(eventRequest2.getEvent().getEventName())).thenReturn(false);
+        when(wordMatcher.detectUnnecessaryWords(eventRequest2.getEvent().getEventName())).thenReturn(false);
+        assertEquals(201, eventRestControllerPublic.createEvent(eventRequest2).getStatusCodeValue());
+        // Есть плохие слова
+        when(wordMatcher.detectBadWords(eventRequest2.getEvent().getEventName())).thenReturn(true);
+        when(wordMatcher.detectUnnecessaryWords(eventRequest2.getEvent().getEventName())).thenReturn(false);
+        assertThrows(EventCreateRequestException.class, ()-> eventRestControllerPublic.createEvent(eventRequest2));
+        // Есть ненужные слова
+        when(wordMatcher.detectBadWords(eventRequest2.getEvent().getEventName())).thenReturn(false);
+        when(wordMatcher.detectUnnecessaryWords(eventRequest2.getEvent().getEventName())).thenReturn(true);
+        assertThrows(EventCheckException.class, ()-> eventRestControllerPublic.createEvent(eventRequest2));
+        // Исключение плохих слов > ненужных слов
+        when(wordMatcher.detectBadWords(eventRequest2.getEvent().getEventName())).thenReturn(true);
+        when(wordMatcher.detectUnnecessaryWords(eventRequest2.getEvent().getEventName())).thenReturn(true);
+        assertThrows(EventCreateRequestException.class, ()-> eventRestControllerPublic.createEvent(eventRequest2));
+
+        // ТОЖЕ САМОЕ, НО ДЛЯ ОПИСАНИЯ МЕРОПРИЯТИЯ
+        // Сброс проверок по названию мероприятия
+        when(wordMatcher.detectBadWords(eventRequest2.getEvent().getEventName())).thenReturn(false);
+        when(wordMatcher.detectUnnecessaryWords(eventRequest2.getEvent().getEventName())).thenReturn(false);
+        // Нет плохих слов
+        when(wordMatcher.detectBadWords(eventRequest2.getEvent().getDescriptionEvent())).thenReturn(false);
+        when(wordMatcher.detectUnnecessaryWords(eventRequest2.getEvent().getDescriptionEvent())).thenReturn(false);
+        assertEquals(201, eventRestControllerPublic.createEvent(eventRequest2).getStatusCodeValue());
+        // Есть плохие слова
+        when(wordMatcher.detectBadWords(eventRequest2.getEvent().getDescriptionEvent())).thenReturn(true);
+        when(wordMatcher.detectUnnecessaryWords(eventRequest2.getEvent().getDescriptionEvent())).thenReturn(false);
+        assertThrows(EventCreateRequestException.class, ()-> eventRestControllerPublic.createEvent(eventRequest2));
+        // Есть ненужные слова
+        when(wordMatcher.detectBadWords(eventRequest2.getEvent().getDescriptionEvent())).thenReturn(false);
+        when(wordMatcher.detectUnnecessaryWords(eventRequest2.getEvent().getDescriptionEvent())).thenReturn(true);
+        assertThrows(EventCheckException.class, ()-> eventRestControllerPublic.createEvent(eventRequest2));
+        // Исключение плохих слов > ненужных слов
+        when(wordMatcher.detectBadWords(eventRequest2.getEvent().getDescriptionEvent())).thenReturn(true);
+        when(wordMatcher.detectUnnecessaryWords(eventRequest2.getEvent().getDescriptionEvent())).thenReturn(true);
+        assertThrows(EventCreateRequestException.class, ()-> eventRestControllerPublic.createEvent(eventRequest2));
     }
 
     @Test
     void updateEvent() {
+        when(eventServiceRest.updateEvent(event.getId(), event)).thenReturn(event);
+        assertEquals(200, eventRestControllerPublic.updateEvent(eventRequest2, 1L).getStatusCodeValue());
+        assertThrows(NullPointerException.class, ()-> eventRestControllerPublic.updateEvent(new EventRequest(), 1L));
     }
 
     @Test
     void deleteEvent() {
+        when(eventServiceRest.getEventById(1L)).thenReturn(event);
+        // OK
+        assertEquals(200, eventRestControllerPublic.deleteEvent(1L).getStatusCodeValue());
+        // Not OK
+        assertEquals(204, eventRestControllerPublic.deleteEvent(3L).getStatusCodeValue());
     }
 
     @Test
     void addEventParticipant() {
+        when(eventServiceRest.addParticipant(1L, 1L)).thenReturn(event);
+        // OK
+        assertEquals(200, eventRestControllerPublic.addEventParticipant(joinRequest).getStatusCodeValue());
+        // Неверный запрос
+        assertThrows(NullPointerException.class, ()-> eventRestControllerPublic.addEventParticipant(emptyJoinRequest));
     }
 
     @Test
     void deleteEventParticipant() {
+        when(eventServiceRest.deleteParticipant(1L, 1L)).thenReturn(event);
+        assertEquals(200, eventRestControllerPublic.deleteEventParticipant(joinRequest).getStatusCodeValue());
+        assertThrows(NullPointerException.class,
+                ()-> eventRestControllerPublic.deleteEventParticipant(emptyJoinRequest));
     }
 }
---
+++ teamup-input/src/test/java/ru/team/up/input/controller/publicController/UserRestControllerPublicTest.java
@@ -0,0 +1,205 @@
+package ru.team.up.input.controller.publicController;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import ru.team.up.core.entity.*;
+import ru.team.up.core.monitoring.service.MonitorProducerService;
+import ru.team.up.input.payload.request.UserRequest;
+import ru.team.up.input.service.UserServiceRest;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+class UserRestControllerPublicTest {
+
+    @Mock
+    private UserServiceRest userServiceRest;
+
+    @Mock
+    private MonitorProducerService monitoringProducerService;
+
+    @InjectMocks
+    private UserRestControllerPublic userRestControllerPublic =
+            new UserRestControllerPublic(userServiceRest, monitoringProducerService);
+
+    private User testUser;
+    private List<User> userList;
+    private Event event;
+    private List<Event> eventList;
+    private UserRequest userRequest;
+
+    @BeforeEach
+    private void setUpEntity() {
+        MockitoAnnotations.openMocks(this);
+
+        userList = new ArrayList<>();
+        eventList = new ArrayList<>();
+
+        User securityUser = mock(User.class);
+        Authentication authentication = mock(Authentication.class);
+        SecurityContext securityContext = mock(SecurityContext.class);
+
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+        when(SecurityContextHolder.getContext().getAuthentication().getPrincipal()).thenReturn(securityUser);
+
+        testUser = User.builder()
+                .id(1L)
+                .firstName("Marina")
+                .lastName("Sysenko")
+                .middleName("Alexsandrovna")
+                .username("test")
+                .email("testemail@gmail.com")
+                .password("1234")
+                .accountCreatedTime(LocalDate.of(2021, 11, 20))
+                .lastAccountActivity(LocalDateTime.of(2021, 11, 20, 19, 0))
+                .city("Volgograd")
+                .birthday(LocalDate.of(1967, 1, 20))
+                .aboutUser("I like to cook")
+                .build();
+
+        event = Event.builder()
+                .id(1L)
+                .eventName("eventName")
+                .descriptionEvent("descriptionEvent")
+                .placeEvent("placeEvent")
+                .timeEvent(LocalDateTime.now())
+                .eventUpdateDate(LocalDate.now())
+                .eventType(new EventType(1L, "eventType"))
+                .authorId(testUser)
+                .status(new Status(1L, "eventStatus"))
+                .build();
+
+        userRequest = UserRequest.builder()
+                .user(testUser)
+                .build();
+    }
+
+    @Test
+    void getUserById() {
+        when(userServiceRest.getUserById(1L)).thenReturn(testUser);
+        // OK
+        assertEquals("Marina", userRestControllerPublic.getUserById(1L).getUserDto().getFirstName());
+        // Not OK
+        assertNotEquals("Masha", userRestControllerPublic.getUserById(1L).getUserDto().getFirstName());
+        // Неверный запрос
+        assertThrows(NullPointerException.class,
+                ()-> userRestControllerPublic.getUserById(4L).getUserDto().getFirstName());
+    }
+
+    @Test
+    void getUserByEmail() {
+        when(userServiceRest.getUserByEmail("testemail@gmail.com")).thenReturn(testUser);
+        // OK
+        assertEquals("Marina",
+                userRestControllerPublic.getUserByEmail("testemail@gmail.com").getUserDto().getFirstName());
+        // Not OK
+        assertNotEquals("Masha",
+                userRestControllerPublic.getUserByEmail("testemail@gmail.com").getUserDto().getFirstName());
+        // Неверный запрос
+        assertThrows(NullPointerException.class,
+                ()-> userRestControllerPublic.getUserByEmail("wrong@gmail.com").getUserDto().getFirstName());
+    }
+
+    @Test
+    void getUserByUsername() {
+        when(userServiceRest.getUserByUsername("Marina")).thenReturn(testUser);
+        // OK
+        assertEquals("Marina",
+                userRestControllerPublic.getUserByUsername("Marina").getUserDto().getFirstName());
+        // Not OK
+        assertNotEquals("Masha",
+                userRestControllerPublic.getUserByUsername("Marina").getUserDto().getFirstName());
+        // Неверный запрос
+        assertThrows(NullPointerException.class,
+                ()-> userRestControllerPublic.getUserByUsername("Masha").getUserDto().getFirstName());
+    }
+
+    @Test
+    void getUsersList() {
+        when(userServiceRest.getAllUsers()).thenReturn(userList);
+        // С пустым списком
+        assertThrows(RuntimeException.class, ()-> userRestControllerPublic.getUsersList());
+        userList.add(testUser);
+        // ОК
+        assertEquals(1, userRestControllerPublic.getUsersList().size());
+        // Not OK
+        assertNotEquals(2, userRestControllerPublic.getUsersList().size());
+    }
+
+    @Test
+    void getEventsByOwnerId() {
+        when(userServiceRest.getEventsByOwnerId(1L)).thenReturn(eventList);
+        // Нет мероприятий
+        assertEquals(0, userRestControllerPublic.getEventsByOwnerId(1L).getEventDtoList().size());
+        eventList.add(event);
+        // ОК
+        assertEquals(1, userRestControllerPublic.getEventsByOwnerId(1L).getEventDtoList().size());
+        // Not OK
+        assertNotEquals(2, userRestControllerPublic.getEventsBySubscriberId(2L).getEventDtoList().size());
+    }
+
+    @Test
+    void getEventsBySubscriberId() {
+        when(userServiceRest.getEventsBySubscriberId(1L)).thenReturn(eventList);
+        // Нет мероприятий
+        assertEquals(0, userRestControllerPublic.getEventsBySubscriberId(1L).getEventDtoList().size());
+        eventList.add(event);
+        // ОК
+        assertEquals(1, userRestControllerPublic.getEventsBySubscriberId(1L).getEventDtoList().size());
+        // Not OK
+        assertNotEquals(2, userRestControllerPublic.getEventsBySubscriberId(2L).getEventDtoList().size());
+    }
+
+    @Test
+    void updateUser() {
+        when(userServiceRest.updateUser(userRequest, testUser.getId())).thenReturn(testUser);
+        // Неверный запрос (пользователь не найден)
+        assertThrows(RuntimeException.class, ()-> userRestControllerPublic.updateUser(userRequest, testUser.getId()));
+        when(userServiceRest.getUserById(testUser.getId())).thenReturn(testUser);
+        // ОК
+        assertEquals(1L, userRestControllerPublic.updateUser(userRequest, testUser.getId()).getId());
+        // Not OK
+        assertNotEquals(2L, userRestControllerPublic.updateUser(userRequest, testUser.getId()).getId());
+    }
+
+    @Test
+    void deleteUserById() {
+        when(userServiceRest.getUserById(1L)).thenReturn(testUser);
+        // OK
+        assertEquals(200, userRestControllerPublic.deleteUserById(1L).getStatusCodeValue());
+        // Not OK
+        assertEquals(204, userRestControllerPublic.deleteUserById(2L).getStatusCodeValue());
+    }
+
+    @Test
+    void getTopUsersListInCity() {
+        when(userServiceRest.getTopUsersInCity("Сочи")).thenReturn(userList);
+        // Пустой список
+        assertEquals(0, userRestControllerPublic.getTopUsersListInCity("Сочи").getUserDtoList().size());
+        userList.add(testUser);
+        // ОК
+        assertEquals(1, userRestControllerPublic.getTopUsersListInCity("Сочи").getUserDtoList().size());
+        // Not OK
+        assertEquals(0, userRestControllerPublic.getTopUsersListInCity("Москва").getUserDtoList().size());
+
+    }
+}
