5a8c88bea8e3f056835d192205f98438a3664df6
Ilia Sadykov <www.ilja-sadykov@mail.ru>
2022-05-13 17:55:29 +0300

    feature/TEAMUP-37 отправка KafkaEventDto на вебсокет в NewAssignEventProcessor

--- teamup-kafka/src/main/java/ru/team/up/kafka/processors/NewAssignEventProcessor.java
+++ teamup-kafka/src/main/java/ru/team/up/kafka/processors/NewAssignEventProcessor.java
@@ -1,9 +1,18 @@
 package ru.team.up.kafka.processors;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import ru.team.up.dto.KafkaEventDto;
 import ru.team.up.dto.KafkaEventTypeDto;
+import ru.team.up.websocket.endpoint.ModeratorAssignEndpoint;
+
+import javax.websocket.DeploymentException;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Процессор для обработки события "Назначение нового мероприятия на модератора"
@@ -11,19 +20,43 @@ import ru.team.up.dto.KafkaEventTypeDto;
 
 @Slf4j
 @Component
-public class NewAssignEventProcessor implements KafkaEventProcessor{
+public class NewAssignEventProcessor implements KafkaEventProcessor {
 
-    /**
-     * Тип событий, которые обрабатывает процессор
-     */
-    private static final KafkaEventTypeDto supportType = KafkaEventTypeDto.NEW_ASSIGN_EVENT;
+    @Value("${websocket.moderator.connect.uri}")
+    private String moderatorConnectUri;
 
     /**
      * Метод обработки события "Назначение нового мероприятия на модератора"
      */
     @Override
     public void perform(KafkaEventDto kafkaEventDto) {
-        //TODO Добавить в параметры необходимые данные из KafkaEventDto
-        //TODO Добавить логику обработки события
+        log.debug("Старт обработки события назначения модератора на проверку ивента");
+        ObjectMapper mapper = new ObjectMapper();
+        String kafkaEventString = null;
+        try {
+            log.debug("Перевод KafkaEventDto в методе NewAssignEventProcessor.perform в строку");
+            kafkaEventString = mapper.writeValueAsString(kafkaEventDto);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        log.debug("Создание эндпоинта и подключение к вебсокету...");
+        try {
+            URI uri = new URI(moderatorConnectUri);
+            ModeratorAssignEndpoint endpoint = new ModeratorAssignEndpoint(uri);
+
+            log.debug("Отправка kafkaEventDto на вебсокет...");
+            endpoint.sendMessage(kafkaEventString);
+        } catch (URISyntaxException e) {
+            log.error("Ошибка в URI в методе perform NewAssignEventProcessor");
+            e.printStackTrace();
+        } catch (DeploymentException e) {
+            log.error("DeploymentException: " + e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
+            log.error("Ошибка при подключении к websocket в методе perform NewAssignEventProcessor");
+            log.debug(e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
