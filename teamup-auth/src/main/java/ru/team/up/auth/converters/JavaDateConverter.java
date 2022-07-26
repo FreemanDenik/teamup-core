a4ac01550b595fce1aee0e382da37941a9f06621
Деник Б <denikvy@gmail.com>
2022-07-26 18:32:16 +0300

    feature/TEAMUP-56 Подправил допустимые форматы дат, добавил тест на класс JavaDateConverter

--- teamup-auth/src/main/java/ru/team/up/auth/converters/JavaDateConverter.java
+++ teamup-auth/src/main/java/ru/team/up/auth/converters/JavaDateConverter.java
@@ -6,27 +6,42 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Date;
 
 import static org.apache.commons.lang3.time.DateUtils.parseDateStrictly;
 
+/**
+ * Класс конвертации формат даты в yyyy-MM-dd
+ */
 public class JavaDateConverter {
+    // Шаблоны форматов из которых мы будем формировать формат yyyy-MM-dd
     private static final String[] formats = {
-            "dd.MM.yyyy",
-            "dd-MM-yyyy",
-            "dd/MM/yyyy",
             "yyyy.MM.dd",
             "yyyy-MM-dd",
             "yyyy/MM/dd",
+            "yyyy-M-dd",
+            "yyyy/M/dd",
+            "dd.M.yyyy",
+            "dd-M-yyyy",
+            "dd/M/yyyy",
+            "dd.MM.yyyy",
+            "dd-MM-yyyy",
+            "dd/MM/yyyy"
     };
 
-    public static LocalDate parserToLocalDate(String date) throws DateConvertException {
+    /**
+     * Метод непосредственно проводящий конвертацию формата даты
+     * @param date
+     * @return LocalDate
+     * */
+    public static LocalDate parserToLocalDate(String date) {
         try {
             Date parseDate = parseDateStrictly(date, formats);
             DateFormat finalFormat = new SimpleDateFormat("yyyy-MM-dd");
             return LocalDate.parse(finalFormat.format(parseDate));
         } catch (ParseException e) {
-            throw new DateConvertException("Ошибка при парсинге даты");
+            throw new DateConvertException("Ошибка при конвертации формата даты в формат yyyy-MM-dd");
         }
     }
 }
